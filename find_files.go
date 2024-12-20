d2dbc0f754964b915832280071c6720521c00786
bopvlk <bopvlk@gmail.com>
2024-12-20 23:33:18 +0200

    fix: fix file path

--- find_files.go
+++ find_files.go
@@ -4,19 +4,20 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"runtime"
 )
 
 func findFilePaths() []string {
-	// pwd of the directory
-	_, filename, _, _ := runtime.Caller(1)
-	dir := filepath.Dir(filename)
+	// Get the current working directory (terminal's directory)
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Slice to hold paths of .sql files
 	var sqlFiles []string
 
 	// Walk through the directory to find .sql files
-	err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
+	err = filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
